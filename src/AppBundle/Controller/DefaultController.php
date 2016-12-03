a28366eea8a2d4f2934ff8eac3ce7ba1650869ee
Jerzy <jerzy.zawadzki@polcode.com>
2016-12-03 13:43:49 +0100

    patch from @snpy

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,24 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\SimplestType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @View\Route("/", name="homepage")
+     * @View\Template
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+    }
+
+    /** @View\Template */
+    public function subAction()
+    {
+        return ['form' => $this->createForm(SimplestType::class, null, ['action' => ''])->createView()];
     }
 }
